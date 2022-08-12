4f1632f9c8a6dc25208d7c7301fdecf64ad0c80b
unknown <zainebb7k9h1@gmail.com>
2022-08-12 15:56:02 +0100

    updating, 55% done

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,4 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, bool=False):
     if problems.__len__() > 5:
         return "Error: Too many problems."
     operators = ["+", "-"]
@@ -11,8 +11,20 @@ def arithmetic_arranger(problems):
                 break
             elif -1 == pos and operators.__len__() == i:
                 return "Error: Operator must be '+' or '-'."
-        
+        try:
+            firstOperandstr = problem[0:pos]
+            secondOperandstr = problem[pos+1::]
+            firstOperand = int(firstOperandstr)
+            secondOperand = int(secondOperandstr)
+            firstDigits = firstOperandstr.__len__()
+            secondDigits = secondOperandstr.__len__()
+            if firstDigits>4 or secondDigits>4:
+                return "Error: Numbers cannot be more than four digits."
+        except ValueError:
+            return "Error: Numbers must only contain digits."
+        print(firstOperand, "\n",problem[pos],secondOperand,"\n")
 
 
-array = ["35+6", "33-9", "44+2"]
+array = ["3475+6", "337-9", "4474-2"]
 print(arithmetic_arranger(array))
+

5bf08c925087c5276166d0a8308fb7dac7bf90ef
unknown <zainebb7k9h1@gmail.com>
2022-08-14 00:09:02 +0100

    finished, (last version), not too much comments

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,15 +1,21 @@
-# import numpy as np y=np.transpose(x)
+
+# when we wanted to transopse a 2d arrayn, we could've used numpy:  import numpy as np y=np.transpose(x)
+# but i chose to write a function
 def transposee(matrix):
     result = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
     return result
 
 def arithmetic_arranger(problems, bool=False):
+    # arranged_problems will be a 2d array where we have each problem digested in each array
     arranged_problems = []
+    # problemsConstruction would hold the first and second operand, the operator and the number of dashes
     problemsConstruction = []
+    # now, we will count the amount of problems supplied into the function, if it's more than 5, we will show an error
     countProblems = problems.__len__()
     if  countProblems > 5:
         return "Error: Too many problems."
     operators = ["+", "-"]
+    # now, because the existence of only one operator from the array: operators is required: we will check it
     for problem in problems:
         i = 0
         for op in operators:
@@ -19,13 +25,18 @@ def arithmetic_arranger(problems, bool=False):
                 break
             elif -1 == pos and operators.__len__() == i:
                 return "Error: Operator must be '+' or '-'."
+        # now that we have an operation with one accepted operator, we will check if it operands got only DIGITS
         try:
-            firstOperandstr = problem[0:pos]
-            secondOperandstr = problem[pos::]
+            firstOpstr = problem[0:pos].split(" ")
+            firstOperandstr = firstOpstr[0]
+            secondOpstr = problem[pos::].split(" ")
+            secondOperandstr = secondOpstr[1]
             firstOperand = int(firstOperandstr)
             secondOperand = int(secondOperandstr)
+            if problem[pos]=="-":
+                secondOperand = (-1)*secondOperand
             firstDigits = firstOperandstr.__len__()
-            secondDigits = secondOperandstr.__len__()-1
+            secondDigits = secondOperandstr.__len__()
             if firstDigits>4 or secondDigits>4:
                 return "Error: Numbers cannot be more than four digits."
         except ValueError:
@@ -34,36 +45,31 @@ def arithmetic_arranger(problems, bool=False):
         problemConstruction = [firstOperand,secondOperand,problem[pos],lines]
         problemsConstruction.append(problemConstruction)
         line1 = firstOperandstr.rjust(lines)
-        line21 = problem[pos].rjust(1)
-        line22 = secondOperandstr[1::].rjust(lines-2)
+        line21 = problem[pos].ljust(0,' ')
+        line22 = secondOperandstr.rjust(lines-1,' ')
         dashes = '-'*lines
-         #arranged_problem =""+line1+"\n"+line21+" "+line22+"\n"+dashes
-        arranged_problem = [line1,line21+" "+line22,dashes]
+        arranged_problem = [line1,line21+line22,dashes]
         arranged_problems.append(arranged_problem)
+    # we need the transposed array of arranged_problems, because we are printing the operations horizontally
     arranged_problemsT = transposee(arranged_problems)
-    print(arranged_problemsT)
-    i=0
-    j=0
-    while(j<countProblems):
+    # in the next loop, we will code the displaying pattern:
+    j = 0
+    while (j < 3):
+        i = 0
         k = 0
-        while(k<3):
-            print(arranged_problemsT[j][k],end='    ')
-            k+=1
-           # if (bool == True and k==2):
-            #    print((str(problemsConstruction[i][0] + problemsConstruction[i][1])).rjust(problemsConstruction[i][3]))
-        i += 1
+        while (k < countProblems):
+            print(arranged_problemsT[j][k], end='    ')
+            k += 1
+            if (bool == True and j==2 and k==countProblems):
+                print("")
+                while(i<countProblems):
+                    print((str(problemsConstruction[i][0] + problemsConstruction[i][1])).rjust(problemsConstruction[i][3]), end='    ')
+                    i += 1
         print("")
         j += 1
+    arranged_problems = ""
+    return arranged_problems
 
 
-
-
-
-
-
-
-
-array = ["32+8", "1-3801", "9999+9999", "523-89"]
-print(arithmetic_arranger(array),True)
 
 
--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-# arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
-for i in range(1, 11):
- print(i, end=" ")
+from arithmetic_arranger import arithmetic_arranger
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))

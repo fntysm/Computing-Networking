63fb8eb610c8cb987a2b404ce4ee2fce39a1a80c
unknown <zainebb7k9h1@gmail.com>
2022-08-20 19:13:17 +0100

    Creating an incoming socket

--- ProxyWebServer/sockets.py
+++ ProxyWebServer/sockets.py
@@ -1,3 +1,24 @@
+import signal
+import socket
+
 class Server:
-    def __init__(self):
-        
+    # creating a socket for the incoming connections, we bind the socket and wait for the client to connect
+    def __init__(self, config):
+        # The signal.signal() function allows defining custom handlers to be executed when a signal is received.
+        # SIGINT is translated into a KeyboardInterrupt exception
+        signal.signal(signal.SIGINT, self.shutdown)
+        # Create a TCP socket, the arguments passed in are the address family and socket type respectively
+        # AF_INET is the Internet address family for IPv4. SOCK_STREAM is the socket type for TCP.
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Re-use the socket
+        # (level, option_name, value: int)
+        # (the socket layer itself, it allows the socket to bind to a port even if in use by another socket ,)
+        self.serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+        # bind the socket to a public host or a port
+        # ADDRESS OF BIND PORT
+        self.serverSocket.bind(config['BIND_PORT'])
+        # Enable a server to accept connections,
+        self.serverSocket.listen(10)
+        self.clients = {}
+
+

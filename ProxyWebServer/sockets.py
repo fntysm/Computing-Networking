742e958cc2dd229e77cc99699af0df8813caed89
unknown <zainebb7k9h1@gmail.com>
2022-08-20 19:41:46 +0100

    Accept client and process

--- ProxyWebServer/sockets.py
+++ ProxyWebServer/sockets.py
@@ -1,5 +1,7 @@
 import signal
 import socket
+import threading
+
 
 class Server:
     # creating a socket for the incoming connections, we bind the socket and wait for the client to connect
@@ -13,7 +15,7 @@ class Server:
         # Re-use the socket
         # (level, option_name, value: int)
         # (the socket layer itself, it allows the socket to bind to a port even if in use by another socket ,)
-        self.serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+        self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # bind the socket to a public host or a port
         # ADDRESS OF BIND PORT
         self.serverSocket.bind(config['BIND_PORT'])
@@ -21,4 +23,15 @@ class Server:
         self.serverSocket.listen(10)
         self.clients = {}
 
-
+    def clientListen(self):
+        while True:
+            # Establishing the connection
+            (clientsocket, clientaddress) = self.serverSocket.accept()
+            # the callable object is the target, name is the threadName
+            # A thread is a separate flow of execution. This means that your program will have two things happening at once.
+            d = threading.Thread(name=self.getClient(clientaddress), target=self.proxyThread,
+                                 args=(clientsocket, clientaddress))
+            # The main purpose of a daemon thread is to execute background task especially in case of some routine periodic task or work.
+            d.setDaemon(True)
+            d.start()
+  

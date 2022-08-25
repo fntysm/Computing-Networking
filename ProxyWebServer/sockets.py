c9403778825a6e59ddd08e2460f8bcb6f26af89c
unknown <zainebb7k9h1@gmail.com>
2022-08-25 23:18:10 +0100

    last lines of code before running

--- ProxyWebServer/sockets.py
+++ ProxyWebServer/sockets.py
@@ -4,8 +4,10 @@ import threading
 import sys
 
 config = {
+    'HOST_NAME': '',
     'BIND_PORT': '',
-    'MAX_REQUEST_LEN': ''
+    'MAX_REQUEST_LEN': '',
+    'CONNECTION_TIMEOUT' : 10
 }
 
 
@@ -24,7 +26,7 @@ class Server:
         self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # bind the socket to a public host or a port
         # ADDRESS OF BIND PORT
-        self.serverSocket.bind(config['BIND_PORT'])
+        self.serverSocket.bind((config['HOST_NAME'], config['BIND_PORT']))
         # Enable a server to accept connections, we chose 10 clients
         self.serverSocket.listen(10)
         self.clients = {}
@@ -35,17 +37,69 @@ class Server:
             (clientsocket, clientaddress) = self.serverSocket.accept()
             # the callable object is the target, name is the threadName
             # A thread is a separate flow of execution. This means that your program will have two things happening at once.
-            d = threading.Thread(name=self.getClient(clientaddress), target=self.proxyThread,
+            d = threading.Thread(name=self.getClient(clientaddress), target=self.proxyTraffic,
                                  args=(clientsocket, clientaddress))
             # The main purpose of a daemon thread is to execute background task especially in case of some routine periodic task or work.
             d.setDaemon(True)
             d.start()
 
-    def proxyTraffic(self, connection):
+    def proxyTraffic(self, connection, clientAddress, config):
         # here we get the request from the browser
         # .recv() is to receive data from TCP and UDP sockets
+        global s
         request = connection.recv(config['MAX_REQUEST_LEN'])
         # here we retrieve the first line
         first_line = request.split('\n')[0]
         url = first_line.split(' ')[1]
+        # to get the address of the requested website
+        httpCheck = url.find('://')
+        if httpCheck==-1:
+            temp = url
+        else:
+            temp = url[(httpCheck+3):]
+        # find the port position
+        portCheck = temp.find(":")
+        # and now we have to find the end of the webserver
+        webserverCheck = temp.find('/')
+        if webserverCheck==-1:
+            webserverCheck = len(temp)
+        webserver = ""
+        port = -1
+        if (portCheck==-1 or webserverCheck<portCheck):
+            # we assign a default port
+            port = 80
+            webserver = temp[:webserverCheck]
+        else:
+            # else we need a specific port
+            port = int((temp[(portCheck+1):])[:webserverCheck-portCheck-1])
+            webserver = temp[:portCheck]
 
+        try:
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.settimeout(config['CONNECTION_TIMEOUT'])
+            s.connect((webserver,port))
+            s.sendall(request)
+            # we have to redirect the server's response to the client
+            while 1:
+                data = s.recv(config['MAX_REQUEST_LEN'])
+                if (len(data)>0):
+                    connection.send(data)
+                else:
+                    print("no connection sent to the client")
+                    break
+            s.close()
+            connection.close()
+        except socket.error as error_msg:
+            print("ERROR OCCURED: ", clientAddress, error_msg)
+            if s:
+                s.close()
+            if connection:
+                connection.close()
+
+    def getClient(self, clientAddress):
+        return "ClientAddr"
+
+    def shutdown(self):
+        # exit the system
+        self.serverSocket.close()
+        sys.exit(0)

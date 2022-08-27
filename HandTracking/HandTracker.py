0ded479efa2bcf748c532beb40a46a85fe30c05f
unknown <zainebb7k9h1@gmail.com>
2022-08-27 17:16:27 +0100

    1.1

--- HandTracking/HandTracker.py
+++ HandTracking/HandTracker.py
@@ -10,32 +10,33 @@ class HandAI:
         self.trackConf = trackConf
         # standard mediapipe code to write
         self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands(self.mode, self.maxHands, self.detectConf, self.trackConf)
+        self.hands = self.mp_hands.Hands()
         self.mpDraw = mp.solutions.drawing_utils
-    def detectHands(self):
-        cam = cv2.VideoCapture(0)
-        success, record = cam.read()
+    def detectHands(self, record):
         # sending our RGB image to the "hands" object
         recordRGB = cv2.cvtColor(record, cv2.COLOR_BGR2RGB)
         # processing after converting the BGR image to RGB
-        results = self.hands.process(recordRGB)
+        self.results = self.hands.process(recordRGB)
         # to check if something on camera is detected
         # print(results.multi_hand_landmarks)
-        if results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
             # if we detected hand(s)
-            for eachHand in results.multi_hand_landmarks:
+            for eachHand in self.results.multi_hand_landmarks:
                 for id, lm in enumerate(eachHand.landmark):
                     print(id, lm)
                     height, width, channel = record.shape
                     # to find the position
                     cx, cy = int(lm.x * width), int(lm.y * height)
-                    cv2.putText(record, str(id + 1), (cx, cy), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 2)
+                    cv2.putText(record, str(id + 1), (cx, cy), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,128), 2)
                 self.mpDraw.draw_landmarks(record, eachHand, self.mp_hands.HAND_CONNECTIONS)
-
-
+        return record
 def HandMain():
+    cam = cv2.VideoCapture(0)
+    Hands = HandAI()
     previousTime = 0
     while True:
+        success, record = cam.read()
+        record = Hands.detectHands(record)
         currentTime = time.time()
         fps = 1 / (currentTime - previousTime)
         previousTime = currentTime

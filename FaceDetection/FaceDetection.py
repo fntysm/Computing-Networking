ee53369eea11e2fca1a7804c44df1e1cf446e3ac
unknown <zainebb7k9h1@gmail.com>
2022-08-28 15:55:50 +0100

    change in plan

--- FaceDetection/FaceDetection.py
+++ FaceDetection/FaceDetection.py
@@ -9,30 +9,30 @@ GREEN_COLOR = (0, 128, 0)
 BLUE_COLOR = (255, 0, 0)
 
 class FaceAI():
-    def __init__(self, minDetectConf=0.5, modelSelection=1):
+    def __init__(self, minDetectConf=0.5):
         self.minDetectConf = minDetectConf
-        self.modelSelection = modelSelection
         self.mpFaceDetection = mp.solutions.face_detection
-        self.FaceDetector = self.mpFaceDetection.FaceDetection()
+        self.FaceDetector = self.mpFaceDetection.FaceDetection(self.minDetectConf)
         self.mpDraw = mp.solutions.drawing_utils
     def faceDetection(self, record):
         results = self.FaceDetector.process(cv2.cvtColor(record, cv2.COLOR_BGR2RGB))
-        circleDrawingSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=1, color=(0, 255, 0))
+        circleDrawingSpec = self.mpDraw.DrawingSpec(thickness=3, circle_radius=1, color=(0, 255, 0))
         lineDrawingSpec = self.mpDraw.DrawingSpec(thickness=1, color=(0, 255, 0))
+        bboxs = []
         if results.detections:
             for id, detection in enumerate(results.detections):
                 # drawing by ourselves
-                # self.mpDraw.draw_detection(record, detection, circleDrawingSpec, lineDrawingSpec)
+                self.mpDraw.draw_detection(record, detection, circleDrawingSpec, lineDrawingSpec)
                 x = detection.location_data.relative_bounding_box.xmin
                 y = detection.location_data.relative_bounding_box.ymin
                 width = detection.location_data.relative_bounding_box.width
                 height = detection.location_data.relative_bounding_box.height
                 w, h, c = record.shape
                 # defining the bounding box
-                bbox = int(x*w),int(y*h),int(width*w),int(height*h)
-                cv2.rectangle(record, bbox, GREEN_COLOR,2)
-                cv2.putText(record, f'{int(detection.score[0]*100)}%', (bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN,
-                            3, (0, 255, 0), 2)
+                bbox = int(x*w), int(y*h), int(width*w), int(height*h)
+                bboxs.append([id, bbox, detection.score])
+                # cv2.rectangle(record, bbox, GREEN_COLOR,2)
+                cv2.putText(record, f'{int(detection.score[0]*100)}%', (bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
 def FaceDet():
     cam = cv2.VideoCapture(0)
     previousTime = 0

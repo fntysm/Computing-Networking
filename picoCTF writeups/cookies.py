f1b9d6f4b2cc1ba23ebe4973d2ee7e2f7f4b6f28
fntysm <zainebb7k9h1@gmail.com>
2022-09-22 09:51:47 +0100

    solved with the help of an old writeup

--- picoCTF writeups/cookies.py
+++ picoCTF writeups/cookies.py
@@ -1,12 +1,16 @@
 import requests
-url = "http://mercury.picoctf.net:29649/"
-for i in range(0,10):
+url = "http://mercury.picoctf.net:29649/check"
+
+for i in range(0, 20):
     text = str(i)
     cookies = {
-        'name' : text
+        'name': text
     }
+
     r = requests.get(url, cookies=cookies)
     result = r.text.split(
         "<p style=\"text-align:center; font-size:30px;\"><b>")[1].split("</b>")[0]
     print("[+] Testing Cookie:{} | Result: {}".format(i, result))
-    print(r.text.split("<code>").split("</code>"))
+
+
+

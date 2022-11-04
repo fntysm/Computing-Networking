a393ce4f17567d6b0f05ee65074583f7a306521b
fntysm <zainebb7k9h1@gmail.com>
2022-11-04 12:47:27 +0100

    import date

--- Implementing Dates and Times/DT.py
+++ Implementing Dates and Times/DT.py
@@ -1,1 +1,8 @@
 import pandas as pd
+from datetime import date
+data = pd.read_csv('Lending-company.csv', index_col='LoanID')
+lendingCoData = data.copy()
+# print(lendingCoData.head())
+# print(lendingCoData['StartDate'].head())
+# to see the datatypes of different columns
+# print(lendingCoData.info())

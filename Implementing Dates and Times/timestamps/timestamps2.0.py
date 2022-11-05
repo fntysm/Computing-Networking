8485d7da06a3a1b469fadfefbb7879ab59aae301
fntysm <zainebb7k9h1@gmail.com>
2022-11-05 20:26:55 +0100

    pytz

--- Implementing Dates and Times/timestamps/timestamps2.0.py
+++ Implementing Dates and Times/timestamps/timestamps2.0.py
@@ -2,5 +2,10 @@ import pytz
 from datetime import datetime
 import pandas as pd
 # converting between timezones
-mydateTime = datetime(year=2022, month=11, day=5, hour=17, minute=57)
-print(mydateTime.tzinfo)
+mydateTime = datetime(year=2022, month=11, day=5, hour=17, minute=57, tzinfo=pytz.UTC)
+tzTime = mydateTime.astimezone(pytz.timezone('US/Central'))
+# print(tzTime)
+# print(pytz.all_timezones)
+tzUSpd = pd.to_datetime(tzTime)
+tzUTC = tzUSpd.tz_convert('UTC')
+print(tzUTC)

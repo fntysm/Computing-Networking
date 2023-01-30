ed34dc1b862baa57ab7b7efb1d2dc26d212412dc
fntysm <zainebb7k9h1@gmail.com>
2023-01-30 13:46:16 +0100

    --

--- Implementing Dates and Times/dates/date.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-myDate = date(2022,11,4)
-bDate = date(2004, 9,22)
-# print(type(myDate))
-# print(f'month: {myDate.month}, year: {myDate.year}, weekday: {myDate.weekday()}, day: {myDate.weekday()}')
-delta = myDate-bDate
-print(myDate + delta)
--- Implementing Dates and Times/times/time.py
+++
@@ -1,4 +0,0 @@
-from datetime import time
-localTime = time(hour=17, minute=53, second=0)
-localTime = time()
-print(localTime)
